876f0bfe9b357d770ec899163c44187e33695843
piedaholic <harsh113300@nitp.ac.in>
2018-12-05 01:34:25 +0100

    Initial commit

---
+++ src/com/jms/queue/Browser.java
@@ -0,0 +1,58 @@
+package com.jms.queue;
+
+import java.util.Enumeration;
+
+import javax.naming.InitialContext;
+                                                                           
+import javax.jms.Queue;
+import javax.jms.Session;
+import javax.jms.Message;
+import javax.jms.QueueSession;
+import javax.jms.QueueBrowser;
+import javax.jms.QueueConnection;
+import javax.jms.QueueConnectionFactory;
+
+public class Browser
+{
+    public static void main(String[] args) throws Exception
+    {
+       // get the initial context
+       InitialContext ctx = new InitialContext();
+                                                                          
+       // lookup the queue object
+       Queue queue = (Queue) ctx.lookup("queue/queue0");
+                                                                          
+       // lookup the queue connection factory
+       QueueConnectionFactory connFactory = (QueueConnectionFactory) ctx.
+           lookup("queue/connectionFactory");
+                                                                          
+       // create a queue connection
+       QueueConnection queueConn = connFactory.createQueueConnection();
+                                                                          
+       // create a queue session
+       QueueSession queueSession = queueConn.createQueueSession(false,
+           Session.AUTO_ACKNOWLEDGE);
+                                                                          
+       // create a queue browser
+       QueueBrowser queueBrowser = queueSession.createBrowser(queue);
+                                                                          
+       // start the connection
+       queueConn.start();
+                                                                          
+       // browse the messages
+       Enumeration<?> e = queueBrowser.getEnumeration();
+       int numMsgs = 0;
+                                                                          
+       // count number of messages
+       while (e.hasMoreElements()) {
+          Message message = (Message) e.nextElement();
+          System.out.println(message);
+          numMsgs++;
+       }
+                                                                          
+       System.out.println(queue + " has " + numMsgs + " messages");
+                                                                          
+       // close the queue connection
+       queueConn.close();
+    }
+}
